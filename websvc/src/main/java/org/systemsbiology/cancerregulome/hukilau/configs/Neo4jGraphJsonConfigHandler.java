750611ea03be40719ae087efb0dc306457a90aa2
Hector Rovira <hrovira.isb@gmail.com>
2011-12-19 17:31:53 -0800

    reduced dependency on external neo4j server; fixed shutdown in tomcat

--- websvc/src/main/java/org/systemsbiology/cancerregulome/hukilau/configs/Neo4jGraphJsonConfigHandler.java
+++ websvc/src/main/java/org/systemsbiology/cancerregulome/hukilau/configs/Neo4jGraphJsonConfigHandler.java
@@ -1,30 +1,23 @@
 package org.systemsbiology.cancerregulome.hukilau.configs;
 
 import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
-import org.neo4j.kernel.EmbeddedGraphDatabase;
-import org.neo4j.server.Bootstrapper;
-import org.neo4j.server.WrappingNeoServerBootstrapper;
-import org.neo4j.server.configuration.EmbeddedServerConfigurator;
+import org.neo4j.kernel.AbstractGraphDatabase;
+import org.neo4j.kernel.EmbeddedReadOnlyGraphDatabase;
 import org.systemsbiology.addama.jsonconfig.JsonConfigHandler;
+import org.systemsbiology.cancerregulome.hukilau.utils.DatabaseShutdownHookThread;
 
 import java.util.Map;
-import java.util.logging.Logger;
 
 import static java.lang.Runtime.getRuntime;
-import static org.neo4j.server.configuration.Configurator.WEBSERVER_ADDRESS_PROPERTY_KEY;
-import static org.neo4j.server.configuration.Configurator.WEBSERVER_PORT_PROPERTY_KEY;
 
 /**
  * @author hrovira
  */
 public class Neo4jGraphJsonConfigHandler implements JsonConfigHandler {
-    private static final Logger log = Logger.getLogger(Neo4jGraphJsonConfigHandler.class.getName());
+    private final Map<String, AbstractGraphDatabase> graphDbsById;
 
-    private final Map<String, EmbeddedGraphDatabase> graphDbsById;
-
-    public Neo4jGraphJsonConfigHandler(Map<String, EmbeddedGraphDatabase> map) {
+    public Neo4jGraphJsonConfigHandler(Map<String, AbstractGraphDatabase> map) {
         this.graphDbsById = map;
     }
 
@@ -37,46 +30,12 @@ public class Neo4jGraphJsonConfigHandler implements JsonConfigHandler {
                     String id = item.getString("id");
                     String location = item.getString("location");
 
-                    EmbeddedGraphDatabase graphDb = new EmbeddedGraphDatabase(location);
+                    AbstractGraphDatabase graphDb = new EmbeddedReadOnlyGraphDatabase(location);
                     graphDbsById.put(id, graphDb);
-
-                    Bootstrapper neoServer = newNeo4jBootstrapper(item, graphDb);
-                    addShutdownHook(graphDb, neoServer);
+                    getRuntime().addShutdownHook(new DatabaseShutdownHookThread(graphDb));
                 }
             }
         }
-    }
-
-    private Bootstrapper newNeo4jBootstrapper(JSONObject item, EmbeddedGraphDatabase graphDb) throws JSONException {
-        if (item.has("webAdminPort") && item.has("webAdminAddress")) {
-            Integer webAdminPort = item.getInt("webAdminPort");
-            String webAdminAddress = item.getString("webAdminAddress");
-
-            EmbeddedServerConfigurator config = new EmbeddedServerConfigurator(graphDb);
-            config.configuration().setProperty(WEBSERVER_PORT_PROPERTY_KEY, webAdminPort);
-            config.configuration().setProperty(WEBSERVER_ADDRESS_PROPERTY_KEY, webAdminAddress);
-
-            Bootstrapper neoServer = new WrappingNeoServerBootstrapper(graphDb, config);
-            neoServer.start();
-            return neoServer;
-        }
-        return null;
-    }
-
-    private void addShutdownHook(final EmbeddedGraphDatabase graphDb, final Bootstrapper neoServer) {
-        getRuntime().addShutdownHook(new Thread() {
-            @Override
-            public void run() {
-                try {
-                    if (neoServer != null) {
-                        neoServer.stop();
-                    }
-                } catch (Exception e) {
-                    log.warning(e.getMessage());
-                }
-                graphDb.shutdown();
-            }
-        });
     }
 
 }
