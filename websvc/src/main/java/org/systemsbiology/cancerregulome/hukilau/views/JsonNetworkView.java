388b45ea2b7cfdc273e62bebe93cbf5adfe44adc
Hector Rovira <hrovira.isb@gmail.com>
2011-12-21 13:14:09 -0800

    adding numberOfNodes and numberOfEdges

--- websvc/src/main/java/org/systemsbiology/cancerregulome/hukilau/views/JsonNetworkView.java
+++ websvc/src/main/java/org/systemsbiology/cancerregulome/hukilau/views/JsonNetworkView.java
@@ -1,5 +1,8 @@
 package org.systemsbiology.cancerregulome.hukilau.views;
 
+
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 import org.systemsbiology.addama.commons.web.views.JsonView;
 
@@ -7,17 +10,24 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.PrintWriter;
 import java.util.Map;
+import java.util.logging.Logger;
 
+import static org.apache.commons.lang.StringUtils.capitalize;
 import static org.systemsbiology.addama.commons.web.utils.HttpIO.getDesiredContentType;
 
 /**
  * @author hrovira
  */
 public class JsonNetworkView extends JsonView {
+    private static final Logger log = Logger.getLogger(JsonNetworkView.class.getName());
+
     @Override
     public void render(Map map, HttpServletRequest request, HttpServletResponse response) throws Exception {
         JSONObject data = (JSONObject) map.get("data");
         if (data == null) data = new JSONObject();
+        addNumberOf(data, "nodes");
+        addNumberOf(data, "edges");
+
         JSONObject dataSchema = (JSONObject) map.get("dataSchema");
         if (dataSchema == null) dataSchema = new JSONObject();
 
@@ -28,5 +38,19 @@ public class JsonNetworkView extends JsonView {
 
         PrintWriter writer = response.getWriter();
         writer.print(json.toString());
+    }
+
+    private void addNumberOf(JSONObject data, String arrayKey) {
+        try {
+            if (!data.has(arrayKey)) {
+                data.put(arrayKey, new JSONArray());
+            }
+
+            JSONArray items = data.getJSONArray(arrayKey);
+            data.put("numberOf" + capitalize(arrayKey), items.length());
+        } catch (JSONException e) {
+            log.warning(e.getMessage());
+        }
+
     }
 }
