94976c6cfb63eb4e4bbabc13d612e78cdd4cf9c4
Hector Rovira <hrovira.isb@gmail.com>
2011-12-22 10:51:45 -0800

    refactored node maps writing to json view

--- websvc/src/main/java/org/systemsbiology/cancerregulome/hukilau/views/JsonNetworkView.java
+++ websvc/src/main/java/org/systemsbiology/cancerregulome/hukilau/views/JsonNetworkView.java
@@ -1,19 +1,18 @@
 package org.systemsbiology.cancerregulome.hukilau.views;
 
 
-import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
 import org.systemsbiology.addama.commons.web.views.JsonView;
+import org.systemsbiology.cancerregulome.hukilau.pojo.NodeMaps;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.PrintWriter;
 import java.util.Map;
 import java.util.logging.Logger;
 
-import static org.apache.commons.lang.StringUtils.capitalize;
+import static org.springframework.web.bind.ServletRequestUtils.getStringParameter;
 import static org.systemsbiology.addama.commons.web.utils.HttpIO.getDesiredContentType;
+import static org.systemsbiology.cancerregulome.hukilau.utils.JsonUtils.*;
 
 /**
  * @author hrovira
@@ -21,36 +20,35 @@ import static org.systemsbiology.addama.commons.web.utils.HttpIO.getDesiredConte
 public class JsonNetworkView extends JsonView {
     private static final Logger log = Logger.getLogger(JsonNetworkView.class.getName());
 
+    public static final String BASE_URI = "BASE_URI";
+    public static final String NODE_MAPS = "NODE_MAPS";
+
     @Override
     public void render(Map map, HttpServletRequest request, HttpServletResponse response) throws Exception {
-        JSONObject data = (JSONObject) map.get("data");
-        if (data == null) data = new JSONObject();
+        String baseUri = (String) map.get(BASE_URI);
+        NodeMaps nodeMaps = (NodeMaps) map.get(NODE_MAPS);
+        String nodeLabel = getStringParameter(request, "nodeLabel", "name");
+        String edgeLabel = getStringParameter(request, "edgeLabel", "");
+
+        log.info("baseUri: " + baseUri);
+        log.info("nodes: " + nodeMaps.numberOfNodes());
+        log.info("relationships: " + nodeMaps.numberOfRelationships());
+        log.info("node label: " + nodeLabel);
+        log.info("edge label: " + edgeLabel);
+
+        JSONObject data = new JSONObject();
+        data.put("nodes", createNodeJSON(baseUri, nodeMaps, nodeLabel));
+        data.put("edges", createEdgeJSON(baseUri, nodeMaps, edgeLabel));
         addNumberOf(data, "nodes");
         addNumberOf(data, "edges");
 
-        JSONObject dataSchema = (JSONObject) map.get("dataSchema");
-        if (dataSchema == null) dataSchema = new JSONObject();
+        JSONObject dataSchema = new JSONObject();
+        dataSchema.put("nodes", nodeSchemaJSON(nodeMaps.getNodeProperties()));
+        dataSchema.put("edges", edgeSchemaJSON(nodeMaps.getRelationshipProperties()));
 
-        JSONObject json = new JSONObject();
-        json.put("data", data);
-        json.put("dataSchema", dataSchema);
+        JSONObject json = new JSONObject().put("data", data).put("dataSchema", dataSchema);
+
         response.setContentType(getDesiredContentType(request, this.getContentType()));
-
-        PrintWriter writer = response.getWriter();
-        writer.print(json.toString());
-    }
-
-    private void addNumberOf(JSONObject data, String arrayKey) {
-        try {
-            if (!data.has(arrayKey)) {
-                data.put(arrayKey, new JSONArray());
-            }
-
-            JSONArray items = data.getJSONArray(arrayKey);
-            data.put("numberOf" + capitalize(arrayKey), items.length());
-        } catch (JSONException e) {
-            log.warning(e.getMessage());
-        }
-
+        response.getWriter().print(json.toString());
     }
 }
