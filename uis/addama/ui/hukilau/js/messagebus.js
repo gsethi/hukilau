31967fda4d1bab12ca818b92750415d3c2f7278e
kleinone <kleinone@systemsbiology.org>
2011-12-29 15:54:53 -0800

    New UI layout. Added node query feature. Issue 3. Issue 10.

--- uis/addama/ui/hukilau/js/messagebus.js
+++ uis/addama/ui/hukilau/js/messagebus.js
@@ -3,3 +3,4 @@ Ext.ns('org.systemsbiology.hukilau.apis.events');
 org.systemsbiology.hukilau.apis.events.MessageBus = new Ext.util.Observable();
 org.systemsbiology.hukilau.apis.events.MessageBus.addEvents('graph_db_selected');
 org.systemsbiology.hukilau.apis.events.MessageBus.addEvents('graph_metadata_loaded');
+org.systemsbiology.hukilau.apis.events.MessageBus.addEvents('node_query_submitted');
