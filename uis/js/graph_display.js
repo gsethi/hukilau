2f85c1242dee12a57c79fd2ff960766a289cf788
Hector Rovira <hrovira.isb@gmail.com>
2012-01-23 13:12:48 -0800

    added messages, cleanup scope

--- uis/js/graph_display.js
+++ uis/js/graph_display.js
@@ -2,33 +2,32 @@ Ext.ns('org.systemsbiology.hukilau.components');
 
 var GraphDisplay = Ext.extend(Object, {
     vis: undefined,
-    container_id: undefined,
 
-    constructor: function(p_container_id) {
-        this.container_id = p_container_id;
+    constructor: function(config) {
+        Ext.apply(this, config);
+
+        GraphDisplay.superclass.constructor.call(this);
     },
 
     doQuery: function(uri) {
-
-        var that = this;
         Ext.Ajax.request({
-            method: "get",
+            method: "GET",
             url: uri,
             success: function(o) {
-                var json = Ext.util.JSON.decode(o.responseText);
-
-                that.redraw(json);
+                this.redraw(Ext.util.JSON.decode(o.responseText));
             },
             failure: function(o) {
                 console.log('Query failed:');
                 console.log(o.responseText);
-            }
+                org.systemsbiology.addama.js.Message.error("Query Failed", o.responseText);
+            },
+            scope: this
         });
     },
 
     redraw: function(json) {
         if (this.vis === undefined) {
-            this.vis = new org.cytoscapeweb.Visualization(this.container_id, {
+            this.vis = new org.cytoscapeweb.Visualization(this.contentEl, {
                 swfPath: "https://informatics-apps.systemsbiology.net/cytoscapeweb_v0.8/swf/CytoscapeWeb",
                 flashInstallerPath: "https://informatics-apps.systemsbiology.net/cytoscapeweb_v0.8/swf/playerProductInstall"
             });            
@@ -42,8 +41,11 @@ var GraphDisplay = Ext.extend(Object, {
     }
 });
 
-org.systemsbiology.hukilau.components.GraphDisplay = new GraphDisplay('c_vis');
+Ext.onReady(function() {
+    org.systemsbiology.hukilau.components.GraphDisplay = new GraphDisplay({contentEl: 'c_vis'});
 
-org.systemsbiology.hukilau.apis.events.MessageBus.on('node_query_submitted',
-    org.systemsbiology.hukilau.components.GraphDisplay.doQuery,
-    org.systemsbiology.hukilau.components.GraphDisplay);
+    org.systemsbiology.hukilau.apis.events.MessageBus.on('node_query_submitted',
+        org.systemsbiology.hukilau.components.GraphDisplay.doQuery,
+        org.systemsbiology.hukilau.components.GraphDisplay);
+
+});
