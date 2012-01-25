6d3f3d941f7c48170fea448368de69450998428e
kleinone <kleinone@systemsbiology.org>
2012-01-24 16:48:58 -0800

    updates

--- uis/js/viewport.js
+++ uis/js/viewport.js
@@ -1,41 +1,63 @@
 Ext.onReady(function() {
+	var graphQueryTabs = new Ext.TabPanel({
+		activeTab: 0,
+		items: [
+			org.systemsbiology.hukilau.apis.panels.NodeQuery
+		]
+	});
 
-    new org.systemsbiology.addama.js.TopBar({contentEl: "c_addama_topbar"});
+	var graphControlPanel = {
+		xtype: 'panel',
+		id: 'graph_control_panel',
+		region: 'west',
+		contentEl: 'c_graph_control',
+		width: 300,
+		layout: 'auto',
+		layoutConfig: {
+			align: 'top'
+		},
+		items: [
+			org.systemsbiology.hukilau.apis.panels.GraphDatabaseSelect,
+			graphQueryTabs,
+			org.systemsbiology.hukilau.apis.panels.QueryResultInfo,
+			org.systemsbiology.hukilau.apis.panels.NodeSchemaSettings,
+			org.systemsbiology.hukilau.apis.panels.EdgeSchemaSettings
+		]
+	};
 
-    var graphQueryTabs = new Ext.TabPanel({
-        activeTab: 0,
-        items: [ org.systemsbiology.hukilau.apis.panels.NodeQuery ]
-    });
+	var graphDisplayPanel = {
+		title: "Graph",
+		contentEl: 'c_vis'
+	};
 
-    var graphControlPanel = {
-        xtype: 'panel',
-        id: 'graph_control_panel',
-        region: 'west',
-        contentEl: 'c_graph_control',
-        width: 300,
-        layout: 'auto',
-        layoutConfig: {
-            align: 'top'
-        },
-        items: [
-            org.systemsbiology.hukilau.apis.panels.GraphDatabaseSelect,
-            graphQueryTabs
-        ]
-    };
+	var queryResultPanel = {
+		title: 'Query Result',
+		layout: 'border',
+		items: [
+			org.systemsbiology.hukilau.apis.panels.nodePropGridPanel,
+			org.systemsbiology.hukilau.apis.panels.edgePropGridPanel
+		]
+	};
 
-    var graphDisplayPanel = {
-        contentEl: 'c_vis',
-        region: 'center'
-    };
+	var dataDisplayPanel = new Ext.TabPanel({
+		region: 'center',
+		id: 'data_display_panel',
+		activeTab: 0,
+		items: [
+			graphDisplayPanel,
+			queryResultPanel
+		]
+	});
+
+	new org.systemsbiology.addama.js.TopBar({contentEl: "c_addama_topbar"});
 
 	new Ext.Viewport({
 		layout: 'border',
 		renderTo: Ext.getBody(),
 		items: [
-            new Ext.Panel({ contentEl: 'c_addama_topbar', region: 'north', height: 27 }),
+			new Ext.Panel({ contentEl: 'c_addama_topbar', region: 'north', height: 27 }),
 			graphControlPanel,
-			graphDisplayPanel
+			dataDisplayPanel
 		]
 	});
-
 });
