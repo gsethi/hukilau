4da474cea3e0eb20c2dc379d929d94217cb6d84d
kleinone <kleinone@systemsbiology.org>
2012-02-03 11:00:50 -0800

    Added tabs for query data grids

--- uis/js/query_handler.js
+++ uis/js/query_handler.js
@@ -1,48 +1,23 @@
 Ext.ns('org.systemsbiology.hukilau.components');
 
-var QueryHandler = Ext.extend(Object, {
-	array_to_uri_map: function(p_array) {
-		var uri_map = {};
-		var n = p_array.length;
+org.systemsbiology.hukilau.components.QueryHandler = Ext.extend(Object, {
+    tab_container: undefined,
+    query_counter: 0,
 
-		for (var i = 0; i < n; i++) {
-			uri_map[p_array[i].uri] = p_array[i];
-		}
+    constructor: function(config) {
+        this.tab_container = config.tab_container;
 
-		return uri_map;
-	},
+        org.systemsbiology.hukilau.apis.events.MessageBus.on('node_query_submitted', this.doNodeQuery, this);
+    },
 
-    doQuery: function(uri) {
+    doNodeQuery: function(uri) {
         var that = this;
-        Ext.Ajax.request({
-            method: "get",
-            url: uri,
-            success: function(o) {
-                var json = Ext.util.JSON.decode(o.responseText);
+        this.query_counter += 1;
                 
-                if (json.data.numberOfEdges > 0 || json.data.numberOfNodes > 0) {
-                	org.systemsbiology.hukilau.apis.events.MessageBus.fireEvent('graph_dataschema_available', json.dataSchema);
-                	org.systemsbiology.hukilau.apis.events.MessageBus.fireEvent('query_result_available', json);
-                }
-            },
-            failure: function(o) {
-                console.log('Query failed:');
-                console.log(o.responseText);
-            }
+        new org.systemsbiology.hukilau.components.QueryResultDisplay({
+            parent_container: that.tab_container,
+            container_title: 'Node Query ' + this.query_counter,
+            uri: uri
         });
     },
-    
-    getCurrentData: function() {
-    	return this.current_data;
-    },
-
-    getCurrentDataSchema: function() {
-    	return this.current_data_schema;
-    }
 });
-
-org.systemsbiology.hukilau.components.QueryHandler = new QueryHandler();
-
-org.systemsbiology.hukilau.apis.events.MessageBus.on('node_query_submitted',
-    org.systemsbiology.hukilau.components.QueryHandler.doQuery,
-    org.systemsbiology.hukilau.components.QueryHandler);
