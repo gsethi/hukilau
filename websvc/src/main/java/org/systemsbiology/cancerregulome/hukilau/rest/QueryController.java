4ef750d356a4a0369939c09c49d910496621c5d4
kleinone <kleinone@systemsbiology.org>
2012-03-02 16:37:01 -0800

    Added numerical range filtering for nodes

--- websvc/src/main/java/org/systemsbiology/cancerregulome/hukilau/rest/QueryController.java
+++ websvc/src/main/java/org/systemsbiology/cancerregulome/hukilau/rest/QueryController.java
@@ -1,35 +1,43 @@
 package org.systemsbiology.cancerregulome.hukilau.rest;
 
+import groovy.json.JsonException;
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.json.JSONException;
 import org.neo4j.graphdb.Node;
 import org.neo4j.graphdb.index.Index;
+import org.neo4j.graphdb.index.IndexHits;
 import org.neo4j.graphdb.index.IndexManager;
 import org.neo4j.kernel.AbstractGraphDatabase;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.servlet.ModelAndView;
 import org.systemsbiology.addama.commons.web.exceptions.InvalidSyntaxException;
 import org.systemsbiology.addama.commons.web.exceptions.ResourceNotFoundException;
 import org.systemsbiology.addama.commons.web.views.JsonItemsView;
 import org.systemsbiology.addama.commons.web.views.JsonView;
+import org.systemsbiology.addama.commons.web.editors.*;
 import org.systemsbiology.addama.jsonconfig.ServiceConfig;
 import org.systemsbiology.addama.jsonconfig.impls.StringPropertyByIdMappingsHandler;
 import org.systemsbiology.cancerregulome.hukilau.configs.Neo4jGraphDbMappingsHandler;
 import org.systemsbiology.cancerregulome.hukilau.configs.NetworkMetadataMappingsHandler;
 import org.systemsbiology.cancerregulome.hukilau.pojo.NodeMaps;
 import org.systemsbiology.cancerregulome.hukilau.views.JsonNetworkView;
+import org.systemsbiology.cancerregulome.hukilau.utils.FilterUtils;
 
 import javax.servlet.http.HttpServletRequest;
+import java.net.URLDecoder;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.Set;
 import java.util.logging.Logger;
+
+import org.apache.lucene.search.*;
+import org.apache.lucene.index.*;
 
 import static org.apache.commons.lang.StringUtils.*;
 import static org.springframework.web.bind.ServletRequestUtils.getIntParameter;
@@ -59,6 +67,11 @@ public class QueryController implements InitializingBean {
         this.serviceConfig.visit(new StringPropertyByIdMappingsHandler(labelsById, "label"));
         this.serviceConfig.visit(new StringPropertyByIdMappingsHandler(nodeIdxById, "nodeIdx"));
         this.serviceConfig.visit(new NetworkMetadataMappingsHandler(networkMetadataById));
+    }
+
+    @InitBinder
+    public void initBinder(WebDataBinder binder) {
+        binder.registerCustomEditor(JSONObject.class, new JSONObjectPropertyEditor());
     }
 
     /*
@@ -171,5 +184,35 @@ public class QueryController implements InitializingBean {
             throw new ResourceNotFoundException(graphDbId);
         }
         return graphDb;
+    }
+
+    @RequestMapping(value = "/**/graphs/{graphDbId}/filter", method = RequestMethod.POST)
+    protected ModelAndView filterNodes(HttpServletRequest request,
+                                       @PathVariable("graphDbId") String graphDbId,
+                                       @RequestParam("filter_config") JSONObject filter_config) throws Exception {
+        BooleanQuery node_query = null;
+
+        try {
+            JSONArray node_filter_list = filter_config.getJSONArray("nodes");
+            node_query = FilterUtils.buildBooleanQuery(node_filter_list);
+        }
+        catch (JSONException e) {
+            throw new InvalidSyntaxException(e.getMessage());
+        }
+        
+        AbstractGraphDatabase graphDB = getGraphDb(graphDbId);
+        IndexManager indexMgr = graphDB.index();
+        Index<Node> nodeIdx = indexMgr.forNodes(nodeIdxById.get(graphDbId));
+
+        IndexHits<Node> hits = nodeIdx.query(node_query);
+        NodeMaps nodeMaps = new NodeMaps();
+
+        for (Node node : hits) {
+            nodeMaps.addNode(node);
+        }
+
+        String baseUri = substringBetween(request.getRequestURI(), request.getContextPath(), "/filter");
+
+        return new ModelAndView(new JsonNetworkView()).addObject(NODE_MAPS, nodeMaps).addObject(BASE_URI, baseUri);
     }
 }
