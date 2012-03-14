cdf78fc618edff68f6ef7122210412268d8e8a87
Hector Rovira <hrovira.isb@gmail.com>
2012-03-14 10:16:38 -0700

    exposed uri configuration

--- uis/js/viewport.js
+++ uis/js/viewport.js
@@ -1,42 +1,32 @@
 Ext.onReady(function() {
-    var workspaceContainer, graphQueryTabs, graphDBSelect, graphControlPanel, addamaToolbar;
-
-    workspaceContainer = new org.systemsbiology.hukilau.components.WorkspaceContainer({
-        region:'center'
+    var workspaceContainer = new org.systemsbiology.hukilau.components.WorkspaceContainer({
+        region: "center",
+        graphStylesUri: "/addama/stores/graphStyles"
     });
 
-    graphQueryTabs = new Ext.TabPanel();
+    var graphQueryTabs = new Ext.TabPanel();
 
-    graphDBSelect = new org.systemsbiology.hukilau.components.queries.GraphDatabaseSelect({
+    var graphDBSelect = new org.systemsbiology.hukilau.components.queries.GraphDatabaseSelect({
         query_tab_panel: graphQueryTabs,
-        workspace_container: workspaceContainer
-    });
-
-    graphControlPanel = {
-        xtype:'panel',
-        region:'west',
-        width:300,
-        layout:'auto',
-        layoutConfig:{
-            align:'top'
-        },
-        items:[
-            graphDBSelect.getPanel(),
-            graphQueryTabs
-        ]
-    };
-
-    addamaToolbar = new org.systemsbiology.addama.js.TopBarToolbar({
-        region:'north',
-        height:30
+        workspace_container: workspaceContainer,
+        graphUri: "/addama/graphs"
     });
 
     new Ext.Viewport({
         layout:'border',
         renderTo:Ext.getBody(),
         items:[
-            addamaToolbar,
-            graphControlPanel,
+            new org.systemsbiology.addama.js.TopBarToolbar({ region:'north', height:30 }),
+            {
+                xtype:'panel',
+                region:'west',
+                width:300,
+                layout:'auto',
+                layoutConfig:{
+                    align:'top'
+                },
+                items:[ graphDBSelect.getPanel(), graphQueryTabs ]
+            },
             workspaceContainer
         ]
     });
